481eb7f2f5656bfebb00bb228c0e572e832a8909
Math012 <math111.0811@gmail.com>
2025-09-03 14:41:05 -0300

    Primeiro commit

---
+++ src/main/java/com/math012/analysis_log_service/AnalysisLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.math012.analysis_log_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisLogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/analysis_log_service/AnalysisLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.analysis_log_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
